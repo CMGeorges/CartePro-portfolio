a122145227feb418d54b020bcdfd2bc6c0f7bd1b
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2025-06-22 21:30:02 -0400

    Add Stripe secret and webhook secret to configuration

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ class Config:
     """Configuration de base de l'application."""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'une-cle-secrete-tres-difficile-a-deviner'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///instance/cards.db'
+    STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+    STRIPE_WEBHOOK_SECRET = os.environ.get('STRIPE_WEBHOOK_SECRET') # Fais de même pour le secret du webhook !
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
